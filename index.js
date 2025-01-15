b20e9f3a8c84d9f2a0e0d58013c5dde07f22032c
Taiwo Omoya <taiwoomoya272@gmail.com>
2025-01-15 11:23:31 -0500

    Append new Category to select element
    - When user add new category the category is
    appended to the select element as an option element

--- index.js
+++ index.js
@@ -1,23 +1,10 @@
-/*import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
-import {
-  getDatabase,
-  ref,
-  push,
-  onValue,
-  remove,
-} from "https://www.gstatic.com/firebasejs/11.1.0/database-app.js";
-*/
 //elements
 const addNewLink = document.getElementById("add-new-link");
 const saveCurrentTab = document.getElementById("save-current-tab");
 const seeSavedLinks = document.getElementById("see-saved-links");
 const selectElement = document.getElementById("categories");
 const newCategoryInput = document.getElementById("new-category");
-/*
-const firebaseConfig = {
-  databaseURL: "https://leads-tracker-app-d1124-default-rtdb.firebaseio.com/",
-};
-*/
+
 
 console.log(document.getElementById("categories")); // Should print the select element
 console.log(document.getElementById("new-category")); // Should print the input element
@@ -33,9 +20,13 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-function appendToSelectElement() {
-  selectElement.add(new Option(newCategoryInput.value));
-}
+
+newCategoryInput.addEventListener("change", function () {
+  let firstLetterCapitalInput = newCategoryInput.value.charAt(0).toUpperCase() + newCategoryInput.value.slice(1)
+  console.log(firstLetterCapitalInput)
+  selectElement.add(new Option(firstLetterCapitalInput));
+  newCategoryInput.value = ""
+})
 
 function handleSubmit(event) {
   //prevent the default behavior where the form refresh
@@ -53,19 +44,7 @@ function handleSubmit(event) {
   //submit it to a backend
 }
 
-//connect the app with the database
-//const app = initializeApp(firebaseConfig);
 
-//setting up the database
-//const database = getDatabase(app);
-//create a reference under which we can start pushing data
-//const referenceInDB = ref(database, "birthdays");
-
-//get a snapshot of the database
-/*onValue(referenceInDB, function (snapshot) {
-  console.log(snapshot);
-});
-*/
 
 //main page eventListeners and function definitions
 addNewLink.addEventListener("click", function () {});
