f37cf5f90e4ec1c3d2615f70279eb1e81d985b85
Taiwo Omoya <taiwoomoya272@gmail.com>
2025-01-15 20:47:55 -0500

    Work on the modalform and added validation to form
    -- Changed the section element to a dialog element
    because of the builtin features that comes with that
    allows me to let the form show and disappear
    
    -- Validation: before a category is added, we check
    whether the category is already there or not.

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 //elements
+const dialog = document.querySelector("dialog")
 const addNewLink = document.getElementById("add-new-link");
 const saveCurrentTab = document.getElementById("save-current-tab");
 const seeSavedLinks = document.getElementById("see-saved-links");
 const selectElement = document.getElementById("categories");
 const newCategoryInput = document.getElementById("new-category");
-
+const form = document.getElementById("form");
 
 console.log(document.getElementById("categories")); // Should print the select element
 console.log(document.getElementById("new-category")); // Should print the input element
@@ -20,14 +21,31 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-
 newCategoryInput.addEventListener("change", function () {
-  let firstLetterCapitalInput = newCategoryInput.value.charAt(0).toUpperCase() + newCategoryInput.value.slice(1)
-  console.log(firstLetterCapitalInput)
+  //trim extra spaces from input
+  const newCategory = newCategoryInput.value.trim()
+
+  
+  //check if the category already exists in the select options
+  const optionExists = Array.from(selectElement.options).some((option) => option.value.toLowerCase() === newCategory.toLowerCase())
+  
+  if(!optionExists){
+    //capitalize the first letter of the input
+    let firstLetterCapitalInput =
+    newCategory.value.charAt(0).toUpperCase() +
+    newCategory.value.slice(1);
+  console.log(firstLetterCapitalInput);
+  
+  //Add the new category as an option before the last position
   const position = selectElement.options.length - 1;
-  selectElement.add(new Option(firstLetterCapitalInput), selectElement.options[position]);
-  newCategoryInput.value = ""
-})
+  selectElement.add(
+    new Option(firstLetterCapitalInput),
+    selectElement.options[position]
+  );
+  }
+  //clear the input field
+  newCategoryInput.value = "";
+});
 
 function handleSubmit(event) {
   //prevent the default behavior where the form refresh
@@ -45,10 +63,15 @@ function handleSubmit(event) {
   //submit it to a backend
 }
 
-
-
 //main page eventListeners and function definitions
-addNewLink.addEventListener("click", function () {});
+addNewLink.addEventListener("click", function () {
+  dialog.showModal()
+});
+
+document.getElementById("closeModal").addEventListener("click", function(){
+  dialog.close();
+})
+
 
 saveCurrentTab.addEventListener("click", function () {});
 
